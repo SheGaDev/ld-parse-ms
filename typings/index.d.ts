76f9795027ed2c8882b10eeb3804f13516fec44f
SheGaDev <61838855+SheGaDev@users.noreply.github.com>
2023-02-09 00:56:50 +0200

    update types

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -9,6 +9,6 @@ export type resultParseMS = {
     milliseconds: number
 }
 
-export function formatTime(milliseconds: number, mark: boolean, args: string[] | []): string;
+export function formatTime(milliseconds: number, mark: boolean, args?: string[] | []): string;
 export function formattingDiscordTimestamp(milliseconds: number, style?: string): string;
-export function parseMilliseconds<T extends keyof resultParseMS>(milliseconds: number, args: T[]): Record<T, number>;
+export function parseMilliseconds<T extends keyof resultParseMS>(milliseconds: number, args?: T[]): Record<T, number>;
